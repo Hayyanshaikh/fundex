932bf2822f04b4b8fff26a58b2f0869c91592864
Hayyanshaikh <hayyanshaikh55@gmail.com>
2024-06-11 15:12:07 +0500

    Home Page completed

--- js/script.js
+++ js/script.js
@@ -2,4 +2,27 @@ $(function() {
   $(".menu_btn").click(function() {
     $(this).toggleClass('open');
   });
+
+  const testimonialSwiper = new Swiper(".testimonial_slider", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
 });
